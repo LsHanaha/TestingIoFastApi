309f41006b091c141f61c2273a855512ff2fae55
LsHanaha <kirik193@yandex.ru>
2021-07-28 16:41:40 +0300

    init commit

---
+++ app/config.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from functools import lru_cache
+
+from pydantic import BaseSettings, AnyUrl
+
+log = logging.getLogger('uvicorn')
+
+
+class Settings(BaseSettings):
+    environment: str = os.getenv('ENVIRONMENT', 'dev')
+    # are app in testing mode now?
+    testing: bool = os.getenv('TESTING', 0)
+    database_url: AnyUrl = os.environ.get("DATABASE_URL")
+
+
+# caching funcs that use frequently. not it only use once
+@lru_cache()
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
+    return Settings()
+
